187259d49c2b55cab79cd86d1d1aac092e730747
ext4cats <ext4cats@farmarrays.com>
2025-01-28 19:33:11 -0300

    add eslint-plugin-react

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,14 @@
 import js from '@eslint/js';
-import globals from 'globals';
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
+    settings: { react: { version: '18.3' } },
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
@@ -16,8 +18,11 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      react,
     },
     rules: {
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
