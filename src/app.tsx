4d42bbdca9c1d026c4d470e17de2b8c2f1a4838f
ext4cats <ext4cats@farmarrays.com>
2025-01-28 20:02:01 -0300

    remove example code

---
+++ src/app.tsx
@@ -0,0 +1,19 @@
+export default function App() {
+  return (
+    <>
+      <h1>dayum</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit hic
+        fugiat natus quo id nobis molestiae eveniet voluptate facilis nulla
+        placeat, et non sunt porro ullam, maxime neque velit cum? Temporibus
+        quibusdam obcaecati tempore voluptatibus voluptatem, dolorem quae
+        nesciunt voluptate recusandae nobis perferendis impedit dignissimos
+        culpa consequuntur ex eligendi deleniti dolorum doloremque, nulla eos
+        possimus autem aliquid? Modi, laudantium labore? Possimus consequatur
+        ducimus veritatis! Nostrum itaque debitis ipsum distinctio nulla facilis
+        et rem eos modi porro vero cumque asperiores voluptatum saepe odit,
+        deleniti amet illo sequi blanditiis? Consectetur, velit consequuntur.
+      </p>
+    </>
+  );
+}
