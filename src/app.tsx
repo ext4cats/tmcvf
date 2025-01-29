ed68bbe0c87f887ac0aa947a81614bbf4c504949
ext4cats <ext4cats@farmarrays.com>
2025-01-29 15:41:52 -0300

    add format selection

--- src/app.tsx
+++ src/app.tsx
@@ -16,9 +16,13 @@ export default function App() {
     const formData = new FormData(event.currentTarget);
     const file = formData.get('file');
     if (!(file instanceof File)) throw new Error('file is not a file');
+    const format = formData.get('format');
+    if (typeof format !== 'string') throw new Error('format must be a string');
+    if (!isValidFormat(format))
+      throw new Error(`unrecognized format: ${format}`);
 
     const ffmpeg = ffmpegRef.current;
-    const output = await transcodeFile(ffmpeg, file);
+    const output = await transcodeFile(ffmpeg, file, format);
     const url = URL.createObjectURL(output);
     const anchor = document.createElement('a');
     anchor.href = url;
@@ -33,19 +37,40 @@ export default function App() {
     <form onSubmit={onSubmit}>
       <label htmlFor="file">Input:</label>
       <input id="file" type="file" name="file" />
+      <select id="format" name="format">
+        {Object.keys(mimeTypes).map((value, idx) => (
+          <option key={idx} value={value}>
+            {value}
+          </option>
+        ))}
+      </select>
       <button type="submit">Convert...</button>
     </form>
   );
 }
 
-async function transcodeFile(ffmpeg: FFmpeg, file: File) {
+async function transcodeFile(ffmpeg: FFmpeg, file: File, format: VideoFormat) {
+  const lastDot = file.name.lastIndexOf('.');
+  const basename = lastDot !== -1 ? file.name.slice(0, lastDot) : file.name;
   const arrayBuf = await file.arrayBuffer();
   const inputBytes = new Uint8Array(arrayBuf);
   await ffmpeg.writeFile(file.name, inputBytes);
-  await ffmpeg.exec(['-i', file.name, 'output.mkv']);
-  const outputBytes = await ffmpeg.readFile('output.mkv');
-  return new File([outputBytes], 'output.mkv', { type: 'video/x-matroska' });
+  await ffmpeg.exec(['-i', file.name, `${basename}.${format}`]);
+  const outputBytes = await ffmpeg.readFile(`${basename}.${format}`);
+  return new File([outputBytes], `${basename}.${format}`, {
+    type: mimeTypes[format],
+  });
 }
+
+type VideoFormat = keyof typeof mimeTypes;
+const mimeTypes = {
+  mkv: 'video/x-matroska',
+  webm: 'video/webm',
+  mp4: 'video/mp4',
+} as const;
+
+const isValidFormat = (format: string): format is VideoFormat =>
+  format in mimeTypes;
 
 function createFFmpeg() {
   const ffmpeg = new FFmpeg();
