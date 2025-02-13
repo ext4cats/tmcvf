8c181da4e1b666bd04bdd979f28a595aac8e1f43
ext4cats <ext4cats@farmarrays.com>
2025-02-13 18:24:33 -0300

    set up tailwind

--- src/app.tsx
+++ src/app.tsx
@@ -1,30 +1,38 @@
+import './app.css';
 import ConverterForm from './components/ConverterForm';
-import ProgressBar from './components/ProgressBar';
 
 export default function App() {
   return (
-    <div>
-      <header>
-        <h1>tmcvf</h1>
-        <p>a tiny video converter powered by ffmpeg.wasm</p>
+    <div className="max-w-xl mx-auto px-4 text-center">
+      <header className="mt-24">
+        <h1 className="text-4xl mb-2">tmcvf</h1>
+        <p className="text-neutral-600">
+          a tiny video converter powered by ffmpeg.wasm
+        </p>
       </header>
-      <main>
+      <main className="my-16">
         <ConverterForm
           onSubmit={(values) => window.alert(JSON.stringify(values, null, 2))}
         />
-        <ProgressBar
-          message="Working..."
-          onCancel={() =>
-            window.alert('You have been cancelled on twitter dot com')
-          }
-        />
       </main>
       <footer>
-        <p>
+        <p className="text-sm text-neutral-600">
           warning: this video converter has no hardware acceleration, and as
           such is both slow and cpu intensive. please use either{' '}
-          <a href="https://www.ffmpeg.org/download.html">native ffmpeg</a> or{' '}
-          <a href="https://handbrake.fr/">handbrake</a> for heavier workloads.
+          <a
+            className="text-indigo-700 hover:underline"
+            href="https://www.ffmpeg.org/download.html"
+          >
+            native ffmpeg
+          </a>{' '}
+          or{' '}
+          <a
+            className="text-indigo-700 hover:underline"
+            href="https://handbrake.fr/"
+          >
+            handbrake
+          </a>{' '}
+          for heavier workloads.
         </p>
       </footer>
     </div>
