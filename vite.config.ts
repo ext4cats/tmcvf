2f1ef1207cd446543099cd8dae873d8bb889db2e
ext4cats <ext4cats@farmarrays.com>
2025-02-24 05:34:29 -0300

    add 404 page

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
+import * as path from 'node:path';
+import * as url from 'node:url';
 import { defineConfig } from 'vite';
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
+  appType: 'mpa',
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        404: path.resolve(__dirname, '404.html'),
+      },
+    },
+  },
   optimizeDeps: {
     exclude: ['@ffmpeg/ffmpeg'],
   },
