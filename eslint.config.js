c06ec21a5ec4a41f4f0af068b87e3f08b60d0edb
ext4cats <ext4cats@farmarrays.com>
2025-03-04 02:18:14 -0300

    add .astro to eslint ignores

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', '.astro'] },
   {
     files: ['**/*.{ts,tsx}'],
     settings: { react: { version: '18.3' } },
